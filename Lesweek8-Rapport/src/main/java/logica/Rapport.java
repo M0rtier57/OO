5f509274b1344c6b64fd5681a55e4a566f9f11c2
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-05 20:55:40 +0200

    Update

--- Lesweek8-Rapport/src/main/java/logica/Rapport.java
+++ Lesweek8-Rapport/src/main/java/logica/Rapport.java
@@ -3,9 +3,7 @@ package logica;
 public class Rapport {
     private Vak[] vakken;
 
-    public Rapport(Vak[] vakken) {
-        this.vakken = vakken;
-    }
+    public Rapport(Vak[] vakken) { this.vakken = vakken;}
 
     public Vak[] getVakken() {
         return this.vakken;
@@ -19,29 +17,28 @@ public class Rapport {
             totaal = totaal + (vakken[i].getScore() * (vakken[i].getStp()));
             totaalstp = totaalstp + (vakken[i].getStp());
         }
+
         double procent = (totaal/(double)totaalstp) * 5;
         return Helper.afronden(procent, 2);
     }
 
     public Graad geefGraad() {
-        if(geefGewogenResultaatProcent() < 0 || geefGewogenResultaatProcent() > 100) {
-            throw new IllegalArgumentException("ongeldig resultaat");
-        }
-        else if(geefGewogenResultaatProcent() < 50) {
-            return Graad.NIET_GESLAAGD;
-        } else if (geefGewogenResultaatProcent() < 65) {
-            return Graad.VOLDOENING;
-        } else if (geefGewogenResultaatProcent() < 75) {
-            return Graad.ONDERSCHEIDING;
-        } else if (geefGewogenResultaatProcent() < 85) {
-            return Graad.GROTE_ONDERSCHEIDING;
-        } else {
-            return Graad.GROOTSTE_ONDERSCHEIDING;
-        }
-
+        if(geefGewogenResultaatProcent() < 0 || geefGewogenResultaatProcent() > 100) throw new IllegalArgumentException("ongeldig resultaat");
+        if(geefGewogenResultaatProcent() < 50) return Graad.NIET_GESLAAGD;
+        if (geefGewogenResultaatProcent() < 65) return Graad.VOLDOENING;
+        if (geefGewogenResultaatProcent() < 75) return Graad.ONDERSCHEIDING;
+        if (geefGewogenResultaatProcent() < 85) return Graad.GROTE_ONDERSCHEIDING;
+        else return Graad.GROOTSTE_ONDERSCHEIDING;
     }
 
     public String toString() {
-        return "";
+        return "" + Helper.afronden(geefGewogenResultaatProcent(), 1) + "% - " + geefGraad().toString().replace('_', ' ');
+    }
+
+    private boolean alleVakkenGeslaagd() {
+        int vakkengeslaagd = 0;
+        for (int i = 0; i < vakken.length; i++) if(vakken[i].getScore() > 10) vakkengeslaagd++;
+        if(vakkengeslaagd == vakken.length) return true;
+        else return false;
     }
 }
