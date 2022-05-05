ee956601ba28d54007cc765ffb731455f6e19639
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-05 21:04:54 +0200

    update

---
+++ Lesweek8-Rapport/src/main/java/presentatie/rapport.java
@@ -0,0 +1,19 @@
+package presentatie;
+
+import javax.swing.*;
+
+public class rapport {
+    private JFormattedTextField punt1;
+    private JLabel vak1;
+    private JFormattedTextField punt2;
+    private JFormattedTextField punt3;
+    private JFormattedTextField punt4;
+    private JFormattedTextField punt5;
+    private JFormattedTextField punt6;
+    private JLabel vak2;
+    private JLabel vak3;
+    private JLabel vak4;
+    private JLabel vak5;
+    private JLabel vak6;
+
+}
