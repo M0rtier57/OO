4b8033562d99c37c3c3c0ad132d055ed7d3f3fe0
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-11 10:12:57 +0200

    first code

---
+++ Figuren/src/main/java/logica/Punt.java
@@ -0,0 +1,49 @@
+package logica;
+
+import java.util.Objects;
+
+public class Punt {
+    private int x;
+    private int y;
+
+    public Punt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double berekenAfstand(Punt punt) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Punt{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Punt punt = (Punt) o;
+        return x == punt.x && y == punt.y;
+    }
+}
