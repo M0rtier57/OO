4abeb32a6c936561191b97985559ba254519eea8
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-11 12:44:47 +0200

    upload

--- Figuren/src/main/java/logica/Cirkel.java
+++ Figuren/src/main/java/logica/Cirkel.java
@@ -13,11 +13,15 @@ public class Cirkel extends Figuur {
     public Cirkel(double straal) {
         this.straal = straal;
         Figuur cirkel = new Figuur();
+        cirkel.berekenOppervlakte();
+        cirkel.berekenOmtrek();
     }
 
     public Cirkel(double straal, Kleur kleur, Kleur kleurRand, int dikteRand) {
         this.straal = straal;
         Figuur cirkel = new Figuur(kleur, kleurRand, dikteRand);
+        cirkel.berekenOppervlakte();
+        cirkel.berekenOmtrek();
     }
 
     public Cirkel(Punt middelpunt, double straal) {
@@ -36,12 +40,10 @@ public class Cirkel extends Figuur {
         return straal;
     }
 
-    @Override
     public double berekenOmtrek() {
         return this.straal * 2 * Math.PI;
     }
 
-    @Override
     public double berekenOppervlakte() {
         return this.straal * this.straal * Math.PI;
     }
