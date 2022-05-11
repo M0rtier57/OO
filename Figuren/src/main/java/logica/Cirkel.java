06edd4b9953d7dddc05a828c8ed0640ac05e7db2
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-11 12:28:42 +0200

    Extends toegevoegd

--- Figuren/src/main/java/logica/Cirkel.java
+++ Figuren/src/main/java/logica/Cirkel.java
@@ -2,83 +2,34 @@ package logica;
 
 import java.util.Objects;
 
-public class Cirkel {
+public class Cirkel extends Figuur {
     private double straal;
 
     public Cirkel() {
         this.straal = 50.0;
         Figuur cirkel = new Figuur() {
-            @Override
-            public double berekenOmtrek() {
-                return Cirkel.this.berekenOmtrek();
-            }
-
-            @Override
-            public double berekenOppervlakte() {
-                return Cirkel.this.berekenOppervlakte();
-            }
-        };
+        Figuur cirkel = new Figuur();
     }
 
     public Cirkel(double straal) {
         this.straal = straal;
-        Figuur cirkel = new Figuur() {
-            @Override
-            public double berekenOmtrek() {
-                return Cirkel.this.berekenOmtrek();
-            }
-
-            @Override
-            public double berekenOppervlakte() {
-                return Cirkel.this.berekenOppervlakte();
-            }
-        };
+        Figuur cirkel = new Figuur();
     }
 
     public Cirkel(double straal, Kleur kleur, Kleur kleurRand, int dikteRand) {
         this.straal = straal;
-        Figuur cirkel = new Figuur(kleur, kleurRand, dikteRand) {
-            @Override
-            public double berekenOmtrek() {
-                return Cirkel.this.berekenOmtrek();
-            }
-
-            @Override
-            public double berekenOppervlakte() {
-                return Cirkel.this.berekenOppervlakte();
-            }
-        };
+        Figuur cirkel = new Figuur(kleur, kleurRand, dikteRand);
     }
 
     public Cirkel(Punt middelpunt, double straal) {
         this.straal = straal;
-        Figuur cirkel = new Figuur() {
-            @Override
-            public double berekenOmtrek() {
-                return Cirkel.this.berekenOmtrek();
-            }
-
-            @Override
-            public double berekenOppervlakte() {
-                return Cirkel.this.berekenOppervlakte();
-            }
-        };
+        Figuur cirkel = new Figuur();
         cirkel.setMiddelpunt(middelpunt);
     }
 
     public Cirkel(Punt middelpunt, double straal, Kleur kleur, Kleur kleurRand, int dikteRand) {
         this.straal = straal;
-        Figuur cirkel = new Figuur(kleur, kleurRand, dikteRand) {
-            @Override
-            public double berekenOmtrek() {
-                return Cirkel.this.berekenOmtrek();
-            }
-
-            @Override
-            public double berekenOppervlakte() {
-                return Cirkel.this.berekenOppervlakte();
-            }
-        };
+        Figuur cirkel = new Figuur(kleur, kleurRand, dikteRand);
         cirkel.setMiddelpunt(middelpunt);
     }
 
@@ -86,10 +37,12 @@ public class Cirkel {
         return straal;
     }
 
+    @Override
     public double berekenOmtrek() {
         return this.straal * 2 * Math.PI;
     }
 
+    @Override
     public double berekenOppervlakte() {
         return this.straal * this.straal * Math.PI;
     }
