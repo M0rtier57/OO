b5a032485af0a57dc155090ca83f527873ef3b88
M0rtier57 <85698830+M0rtier57@users.noreply.github.com>
2022-05-25 12:42:04 +0200

    Update

--- Lesweek8-Rapport/src/main/java/data/IO.java
+++ Lesweek8-Rapport/src/main/java/data/IO.java
@@ -1,4 +1,12 @@
 package data;
 
+import logica.Student;
+
+import java.util.*;
+
 public class IO {
+    public static List<Student> leesStudenten(String source) {
+
+        return null;
+    }
 }
--- Lesweek8-Rapport/src/main/java/logica/Opleiding.java
+++ Lesweek8-Rapport/src/main/java/logica/Opleiding.java
@@ -1,13 +1,16 @@
 package logica;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Opleiding {
     private String naam;
-    private List<Student> studenten;
+    private List<Student> studenten = new ArrayList<>();
 
     public Opleiding(String naam) {
-        this.naam = naam;
+        if(naam != null && naam != "") {
+            this.naam = naam;
+        } else throw new IllegalArgumentException("Ongeldige naam voor de opleiding");
     }
 
     public List<Student> getStudenten() {
@@ -15,10 +18,26 @@ public class Opleiding {
     }
 
     public void voegStudentToe(Student student) {
-        this.studenten.add(student);
+        if(studenten.contains(student)) {
+            studenten.remove(student);
+            studenten.add(student);
+        } else {
+            studenten.add(student);
+        }
+
     }
 
     public void verwijderStudent(Student student) {
-        this.studenten.remove(student);
+        studenten.remove(student);
+    }
+
+    public ArrayList<Student> geefStudentenMetGraad(Graad graad) {
+        ArrayList<Student> graadStudenten = new ArrayList<>();
+        for(Student s : studenten) {
+            if(s.getRapport().geefGraad() == graad) {
+                graadStudenten.add(s);
+            }
+        }
+        return graadStudenten;
     }
 }
